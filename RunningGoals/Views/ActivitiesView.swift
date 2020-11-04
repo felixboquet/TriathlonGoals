dfdc4b68628e74b5e1ec2d6561acecd4555bb6af
Félix Boquet <fboquet31@gmail.com>
2020-11-04 08:20:09 +0100

    Add ActivitiesView file

---
+++ RunningGoals/Views/ActivitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivitiesView.swift
+//  RunningGoals
+//
+//  Created by Féfé on 03/11/2020.
+//
+
+import SwiftUI
+
+struct ActivitiesView: View {
+    
+    
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
